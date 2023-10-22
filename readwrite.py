925e06cdcacac06d91378652abc9d4b72bfb4bff
KamilMachalowski <kamil.machalowski@gmail.com>
2023-10-23 00:04:24 +0200

    try except - Error handling

--- readwrite.py
+++ readwrite.py
@@ -21,28 +21,36 @@ while True:
             print(f"{index + 1}. {item}")
 
     elif user_action.startswith("edit"):
-        number_of_todo = int(user_action[5:]) - 1
+        try:
+            number_of_todo = int(user_action[5:]) - 1
 
-        with open("readwrite.txt", "r") as file:
-            todos = file.readlines()
+            with open("readwrite.txt", "r") as file:
+                todos = file.readlines()
 
-        todos[number_of_todo] = input("Enter new todo: ") + "\n"
+            todos[number_of_todo] = input("Enter new todo: ") + "\n"
 
-        with open("readwrite.txt", "w") as file:
-            file.writelines(todos)
+            with open("readwrite.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
 
     elif user_action.startswith("complete"):
-        number_of_todo = int(user_action[8:]) - 1
+        try:
+            number_of_todo = int(user_action[8:]) - 1
 
-        with open("readwrite.txt", "r") as file:
-            todos = file.readlines()
+            with open("readwrite.txt", "r") as file:
+                todos = file.readlines()
 
-        removed_element = todos.pop(number_of_todo).strip('\n')
+            removed_element = todos.pop(number_of_todo).strip('\n')
 
-        with open("readwrite.txt", "w") as file:
-            file.writelines(todos)
+            with open("readwrite.txt", "w") as file:
+                file.writelines(todos)
 
-        print(f"Successfully removed: {removed_element}")
+            print(f"Successfully removed: {removed_element}")
+        except IndexError:
+            print("There is no item with that number")
+            continue
 
     elif user_action.startswith("exit"):
         break
