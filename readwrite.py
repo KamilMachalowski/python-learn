2f18533cf240486c2670bfda2b2f220c5bad246b
Kamil Machalowski <kamil.machalowski@gmail.com>
2023-10-26 10:57:12 +0200

    modules

--- readwrite.py
+++ readwrite.py
@@ -1,22 +1,4 @@
-def get_todos(filepath="readwrite.txt"):
-    """
-    Read the text file and
-    return the list of to-do items
-    """
-    with open(filepath, "r") as file:
-        todos_list = file.readlines()
-    return todos_list
-
-
-def write_todos(todos_arg, filepath="readwrite.txt"):
-    """
-    Write the to-do items list in the file.
-    :param todos_arg: list with to-do
-    :param filepath: filepath to file to read
-    """
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
-
+import readwrite_modules
 
 while True:
     user_action = input("Type add [text], show, edit [number], complete [number] or exit: ").strip()
@@ -24,14 +6,14 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:] + "\n"
 
-        todos = get_todos()
+        todos = readwrite_modules.get_todos()
 
         todos.append(todo)
 
-        write_todos(todos)
+        readwrite_modules.write_todos(todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos()
+        todos = readwrite_modules.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -41,11 +23,11 @@ while True:
         try:
             number_of_todo = int(user_action[5:]) - 1
 
-            todos = get_todos()
+            todos = readwrite_modules.get_todos()
 
             todos[number_of_todo] = input("Enter new todo: ") + "\n"
 
-            write_todos(todos)
+            readwrite_modules.write_todos(todos)
         except ValueError:
             print("Your command is not valid")
             continue
@@ -54,11 +36,11 @@ while True:
         try:
             number_of_todo = int(user_action[8:]) - 1
 
-            todos = get_todos()
+            todos = readwrite_modules.get_todos()
 
             removed_element = todos.pop(number_of_todo).strip('\n')
 
-            write_todos(todos)
+            readwrite_modules.write_todos(todos)
 
             print(f"Successfully removed: {removed_element}")
         except IndexError:
---
+++ readwrite_modules.py
@@ -0,0 +1,18 @@
+def get_todos(filepath="readwrite.txt"):
+    """
+    Read the text file and
+    return the list of to-do items
+    """
+    with open(filepath, "r") as file:
+        todos_list = file.readlines()
+    return todos_list
+
+
+def write_todos(todos_arg, filepath="readwrite.txt"):
+    """
+    Write the to-do items list in the file.
+    :param todos_arg: list with to-do
+    :param filepath: filepath to file to read
+    """
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
