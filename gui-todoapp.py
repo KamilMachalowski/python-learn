cd7d12a54f4b1d8a66b111f32854b5283de580a5
KamilMachalowski <kamil.machalowski@gmail.com>
2023-11-19 22:12:22 +0100

    implement edit and update

--- gui-todoapp.py
+++ gui-todoapp.py
@@ -4,9 +4,11 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=tdm.get_todos(), key="todos", enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My TODO APP",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -17,9 +19,21 @@ while True:
     match event:
         case "Add":
             todos = tdm.get_todos()
-            new_todo = values['todo'] + "\n"
+            new_todo = values['todo']
             todos.append(new_todo)
             tdm.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
+
+            todos = tdm.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
+            tdm.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
         case sg.WIN_CLOSED:
             break
 
